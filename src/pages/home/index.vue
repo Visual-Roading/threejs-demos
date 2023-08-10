9b206a81a9639ca8df1d9c907ac3b06cae206957
SanQY <roading2022@outlook.com>
2023-08-10 21:12:44 +0800

    feat: add home page

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,7 +2,23 @@
 
 </script>
 <template>
-  <div>This is home page</div>
+  <div class="home-pg">
+    <h2 class="title">This is a project for vue demos.</h2>
+    <img src="snapshot.png" class="snapshot" style="width: 100%;height: 100%;"/>
+  </div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+.home-pg {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .title {
+    margin: 0;
+    padding: 20px;
+  }
+  .snapshot {
+    flex: 1;
+  }
+}
+
 </style>
